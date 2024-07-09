28ccf3c0024a8c87a6f8903d1fd304077dbbd16a
jaganank <naveenj_kumar@rediffmail.com>
2024-07-10 04:38:30 +0530

    "updated all changes"

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='DiamondPricePrediction',
+    version='0.0.1',
+    author='Naveen Kumar',
+    author_email='naveenj_kumar@rediffmail.com',
+    packages=find_packages()
+)
